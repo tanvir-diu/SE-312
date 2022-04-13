6970898848095f6d304874e61bcca4579315a77d
Habibur Rahman <76900910+hrebon@users.noreply.github.com>
2022-04-13 09:21:19 -0600

    Add files via upload

---
+++ Namta.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int number,sum;
+    printf("Enter the value: ");
+    scanf("%d", &number);
+
+    for(int i=1;i<=10;i++){
+        sum = number * i;
+        printf("%d * %d = %d\n",number,i,sum);
+    }
+}
