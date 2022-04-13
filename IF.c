d2990b7a2d992d8cb59810ad86f91a1ac50f4551
Habibur Rahman <76900910+hrebon@users.noreply.github.com>
2022-04-13 09:17:00 -0600

    create mutant
    line number 2,6,10,12 . these lines create a mutant.

--- IF.c
+++ IF.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main()
+float main()
 {
     int age;
     age=38;
-    if(age>10)
-
+    if(age<10)
+        
     {
     printf("age is greater than ten ");
+        continue;
     }
-    return 0;
+    return 1;
 
 }
